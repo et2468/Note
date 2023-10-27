cfcfe96297e2b4d06ec989bebd0349cbd815f26c
csm <csm66865407@gmail.com>
2023-10-27 21:00:44 +0900

    README.md 자동생성 프로그램 제작

---
+++ utils/check_files.py
@@ -0,0 +1,24 @@
+import os
+
+def create_readme():
+    readme_startline = "# 개발 공부를 기록한 통합 레파지토리입니다.\n\n"
+    readme_treeline = generate_readme_treeline("./", 2)
+    readme = readme_startline + readme_treeline
+
+    with open("./README.md", 'w', encoding='utf-8') as f:
+        f.write(readme)
+
+def generate_readme_treeline(directory, depth):
+    readme_treeline = ""
+    for path in os.listdir(directory):
+        full_path = os.path.join(directory, path)
+        if os.path.isdir(full_path) and not path.endswith(("img", "git")):
+            readme_treeline += f"{'#' * depth} {path}\n"
+            readme_treeline += generate_readme_treeline(full_path, depth + 1)
+        elif os.path.isfile(full_path):
+            file_name = os.path.basename(full_path)
+            readme_treeline += f"- [{file_name}]({full_path})\n"
+    return readme_treeline
+
+if __name__ == "__main__":
+    create_readme()

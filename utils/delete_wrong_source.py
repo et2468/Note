8f860e57f23f34cdd62d61a6fe02216d7150899c
csm <csm66865407@gmail.com>
2023-10-28 02:28:58 +0900

    게시글 잘못된 자료출처 삭제 프로그램 생성

--- utils/delete_wrong_source.py
+++ utils/delete_wrong_source.py
@@ -1,23 +1,25 @@
 import os
 
-def insert_source(explain, source_title, source_url, img_url):
+def delete_source(explain, source_title, source_url, img_url):
     # 참고 자료 정보 설정
 
     source = generate_source(explain, source_title, source_url, img_url)
 
     for file in os.listdir():
         # 파일이면서 source가 이미 없는 경우에만 작업 수행
-        if os.path.isfile(file) and not is_source_in_file(file, source):
-            print(f"{file}에 source 추가")
+        if os.path.isfile(file) and is_source_in_file(file, source):
+            print(f"{file}에 source 삭제")
             with open(file, 'r', encoding='utf-8') as f:
-                lines = f.readlines()
+                temp = f.read()
+                print(temp)
+                # lines = f.readlines()
 
-            # 2번째 줄에 source를 추가
-            lines.insert(1, source)
+            # # 2번째 줄에 source를 추가
+            # lines.insert(1, source)
 
-            with open(file, 'w', encoding='utf-8') as f:
-                f.writelines(lines)
-            break
+            # with open(file, 'w', encoding='utf-8') as f:
+            #     f.writelines(lines)
+            # break
 
 def is_source_in_file(file, source):
     # 파일에 source가 이미 있는지 확인
@@ -47,15 +49,14 @@ if __name__ == "__main__":
 
     # 해당 디렉토리의 파일들에 출처 정보 삽입
     PLZ_INSERT_EXPLAIN = "해당 내용은 다음 강의를 참고하여 정리하였습니다."
-    PLZ_INSERT_SOURCE_TITLE = "스프링 핵심 원리 - 기본편"
-    PLZ_INSERT_SOURCE_URL = "https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8/dashboard"
-    PLZ_INSERT_SOURCE_IMG_URL = "https://cdn.inflearn.com/public/courses/325969/cover/2868c757-5886-4508-a140-7cb68a83dfd8/325969-eng.png"
+    PLZ_INSERT_SOURCE_TITLE = "실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발"
+    PLZ_INSERT_SOURCE_URL = "https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8-JPA-%ED%99%9C%EC%9A%A9-1/dashboard"
+    PLZ_INSERT_SOURCE_IMG_URL = "https://cdn.inflearn.com/public/courses/324119/course_cover/07c45106-3cfa-4dd6-93ed-a6449591831c/%E1%84%80%E1%85%B3%E1%84%85%E1%85%AE%E1%86%B8%205%20%E1%84%87%E1%85%A9%E1%86%A8%E1%84%89%E1%85%A1%204.png"
 
 
 
     # 현재 디렉토리가 맞는지 확인 (파일이 잘못 입력될 수도 있으니 꼭 확인)
-    PLZ_INSERT_DIRECTORY_NAME = "스프링의 핵심 원리"
+    PLZ_INSERT_DIRECTORY_NAME = "JPA 기본"
     check_directory(PLZ_INSERT_DIRECTORY_NAME)
 
-    
-    insert_source(PLZ_INSERT_EXPLAIN, PLZ_INSERT_SOURCE_TITLE, PLZ_INSERT_SOURCE_URL, PLZ_INSERT_SOURCE_IMG_URL)
+    delete_source(PLZ_INSERT_EXPLAIN, PLZ_INSERT_SOURCE_TITLE, PLZ_INSERT_SOURCE_URL, PLZ_INSERT_SOURCE_IMG_URL)
